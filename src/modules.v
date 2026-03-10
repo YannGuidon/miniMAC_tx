3ea970d7e40c8748efc0f536e52071d95b439b9a
YannGuidon <github@f-cpu.org>
2026-03-11 00:27:34 +0100

    X vs Y in modules.v

--- src/modules.v
+++ src/modules.v
@@ -226,7 +226,7 @@ endmodule
 
 module or16(
   input  wire [15:0] A,
-  output wire        X
+  output wire        Y
 );
   wire [3:0] t;
 
@@ -234,7 +234,7 @@ module or16(
   sg13g2_nor4_1 nor1(.Y(t[1]), .A(A[ 4]), .B(A[ 5]), .C(A[ 6]), .D(A[ 7]));
   sg13g2_nor4_1 nor2(.Y(t[2]), .A(A[ 8]), .B(A[ 9]), .C(A[10]), .D(A[11]));
   sg13g2_nor4_1 nor3(.Y(t[3]), .A(A[12]), .B(A[13]), .C(A[14]), .D(A[15]));
-  sg13g2_nand4_1 nand4(.Y(X), .A(t[0]), .B(t[1]), .C(t[2]), .D(t[3]));
+  sg13g2_nand4_1 nand4(.Y(Y), .A(t[0]), .B(t[1]), .C(t[2]), .D(t[3]));
 endmodule
 
 module nor16(
@@ -247,6 +247,6 @@ module nor16(
   sg13g2_nor4_1 nor1(.Y(t[1]), .A(A[ 4]), .B(A[ 5]), .C(A[ 6]), .D(A[ 7]));
   sg13g2_nor4_1 nor2(.Y(t[2]), .A(A[ 8]), .B(A[ 9]), .C(A[10]), .D(A[11]));
   sg13g2_nor4_1 nor3(.Y(t[3]), .A(A[12]), .B(A[13]), .C(A[14]), .D(A[15]));
-  sg13g2_and4_1 and4(.Y(X), .A(t[0]), .B(t[1]), .C(t[2]), .D(t[3]));
+  sg13g2_and4_1 and4(.X(X), .A(t[0]), .B(t[1]), .C(t[2]), .D(t[3]));
 endmodule
 
