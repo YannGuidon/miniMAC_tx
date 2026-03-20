bb036a1ddd21430780f16eb524df04251e631be8
YannGuidon <github@f-cpu.org>
2026-03-20 11:47:34 +0100

    Update gPEAC18.v

--- src/gPEAC18.v
+++ src/gPEAC18.v
@@ -5,6 +5,15 @@
   See  https://hackaday.io/project/178998 
 */
 
+module ConstOrPass(
+    input  wire [17:0] A,
+    input  wire C,
+    output wire [17:0] X  
+);
+  assign X = A;
+
+endmodule
+
 
 /* a 18-bit adder, I have no mapped/optimised version available (yet)
    and I have no time left for such detail */
@@ -12,6 +21,7 @@ module Add18(
     input  wire [17:0] A,
     input  wire [17:0] B, 
     output wire [17:0] S
+  // carry out missing ?
 );
-  assign S <= A + B;
+  assign S = A + B;
 endmodule
