fc8b8868ac01957599c0f24f79de4bae67d2e8af
YannGuidon <github@f-cpu.org>
2026-03-21 06:19:44 +0100

    Update gPEAC18.v

--- src/gPEAC18.v
+++ src/gPEAC18.v
@@ -28,21 +28,13 @@ module Compare_modulus(
   (* keep *) sg13g2_and3_1 L4a3_3(.X( X), .A(t1),    .B(t2),    .C(t7));
 endmodule
 
-/* modules.v already defines the registers A, B, X, Y & T
-dffen_x18(
-  input  wire clk,
-  input  wire rst,
-  input  wire en,
-  input  wire [17:0] D,
-  output wire [17:0] Q
-);
-*/
 
 /*
     modulus: std_ulogic_vector(17 downto 0) := "111111000001000010"; -- 258114; 
-    adjust : std_ulogic_vector(17 downto 0) := "000000111110111110"; -- 4030 = 262144 - modulus;
+    adjust : std_ulogic_vector(17 downto 0) := "000000111110111110"; -- 4030 = 262144 - modulus;  => inversion sur autre opérande
 */
 
+/*
 //  INIT_X : std_ulogic_vector(17 downto 0) := "101101101110110111"; -- 187319
 module ConstX_OrPass(
     input  wire [17:0] A,
@@ -53,54 +45,20 @@ module ConstX_OrPass(
   (* keep *) sg13g2_inv_4  inv(.Y(D), .A(C));
   (* keep *) sg13g2_or2_1  cstxR(.X(X[17]), .A(A[17]), .B(C));
   (* keep *) sg13g2_and2_1 cstxQ(.X(X[16]), .A(A[16]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxP(.X(X[15]), .A(A[15]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxO(.X(X[14]), .A(A[14]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxN(.X(X[13]), .A(A[13]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxM(.X(X[12]), .A(A[12]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxL(.X(X[11]), .A(A[11]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxK(.X(X[10]), .A(A[10]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxJ(.X(X[ 9]), .A(A[ 9]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxI(.X(X[ 8]), .A(A[ 8]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxH(.X(X[ 7]), .A(A[ 7]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxG(.X(X[ 6]), .A(A[ 6]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxF(.X(X[ 5]), .A(A[ 5]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxE(.X(X[ 4]), .A(A[ 4]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxD(.X(X[ 3]), .A(A[ 3]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxC(.X(X[ 2]), .A(A[ 2]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxB(.X(X[ 1]), .A(A[ 1]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxA(.X(X[ 0]), .A(A[ 0]), .B(C));
+....
 endmodule
+*/
 
-//  INIT_Y : std_ulogic_vector(17 downto 0) := "011 011 0101 0110 1101"  -- 111981
-module ConstY_OrPass(
+module ConstAdjOrPass(
     input  wire [17:0] A,
     input  wire C,
     output wire [17:0] X  
 );
-  wire D;
-  (* keep *) sg13g2_inv_4  inv(.Y(D), .A(C));
-  (* keep *) sg13g2_and2_1 cstxR(.X(X[17]), .A(A[17]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxQ(.X(X[16]), .A(A[16]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxP(.X(X[15]), .A(A[15]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxO(.X(X[14]), .A(A[14]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxN(.X(X[13]), .A(A[13]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxM(.X(X[12]), .A(A[12]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxL(.X(X[11]), .A(A[11]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxK(.X(X[10]), .A(A[10]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxJ(.X(X[ 9]), .A(A[ 9]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxI(.X(X[ 8]), .A(A[ 8]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxH(.X(X[ 7]), .A(A[ 7]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxG(.X(X[ 6]), .A(A[ 6]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxF(.X(X[ 5]), .A(A[ 5]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxE(.X(X[ 4]), .A(A[ 4]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxD(.X(X[ 3]), .A(A[ 3]), .B(C));
-  (* keep *) sg13g2_or2_1  cstxC(.X(X[ 2]), .A(A[ 2]), .B(C));
-  (* keep *) sg13g2_and2_1 cstxB(.X(X[ 1]), .A(A[ 1]), .B(D));
-  (* keep *) sg13g2_or2_1  cstxA(.X(X[ 0]), .A(A[ 0]), .B(C));
+  assign X = A;
+
 endmodule
 
-
-module ConstOrNeg(
+module ConstModOrNeg(
     input  wire [17:0] A,
     input  wire C,
     output wire [17:0] X  
